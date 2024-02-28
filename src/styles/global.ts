c52f598d5fd88bac28902edac2d6d43bac094db3
felipe <felipe.silva.santos.lawliet1@gmail.com>
2024-02-28 16:38:11 -0300

    first commit

---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components'
+import { mixins } from './mixins'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: none;
+    box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
+  }
+
+  body {
+    background: ${(props) => props.theme.background};
+    color: ${(props) => props.theme.colors['base-text']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    ${mixins.fonts.textM}
+  }
+
+  button {
+    border: none;
+    cursor: pointer;
+  }
+`
